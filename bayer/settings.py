1e3b61ba6ab031f98486f21dafa92e8bb9f4d2f4
Shreenath Bharadwaj <bharadwajshreenath@gmail.com>
2021-11-26 16:12:06 +0530

    deployment 2

--- bayer/settings.py
+++ bayer/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9-k%wu=&mel!nbx8tv5%m8-aq8)khgj50ats21yvj337(d*a&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bayer-hackethon.herokuapp.com']
 
 
 # Application definition
